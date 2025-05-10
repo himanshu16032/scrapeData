e06009b96b99abd1a47a52c9e787c3ac59f96173
Himanshu Sehrawat2 <himanshusehrawat16032000@gmail.com>
2025-05-10 19:55:38 +0530

    trying to use chromium

--- Service/MyntraDataScrapeService.py
+++ Service/MyntraDataScrapeService.py
@@ -54,7 +54,7 @@ playwright_context = {
 async def init_browser():
     pw = await async_playwright().start()
     # Launch Chromium headless with sandbox disabled
-    browser = await pw.firefox.launch(
+    browser = await pw.chromium.launch(
         headless=True,
         args=[
             "--no-sandbox",
